55c398afc0e522f1b9e4b396f3063c91967190cb
Oloyade <oloyade.wakilu@gmail.com>
2023-10-25 11:07:12 +0100

    update

--- Assignments/Assignment/Soln.py
+++ Assignments/Assignment/Soln.py
@@ -8,13 +8,13 @@ The program should prompt the user for the temperature value
 
 # Solution
 #Input temperature in Celsius
-celsius = float(input("Enter temperature in Celsius: "))
+# celsius = float(input("Enter temperature in Celsius: "))
 
-# Convert Celsius to Fahrenheit
-fahrenheit = (celsius * 1.835) + 32
+# # Convert Celsius to Fahrenheit
+# fahrenheit = (celsius * 1.835) + 32
 
-# Result
-print(f"{celsius} degrees Celsius is equal to {fahrenheit} degrees Fahrenheit ")
+# # Result
+# print(f"{celsius} degrees Celsius is equal to {fahrenheit} degrees Fahrenheit ")
 
 
 # Assignment 2
@@ -29,13 +29,13 @@ Distance > 500 miles: $10
 '''
 
 # Solution 1
-Distance = float(input("Enter the distance of your trip in miles: "))
-if Distance <= 100:
-    print("The cost of your trip is $5")
-elif Distance <=500:
-    print("The cost of your trip is $8")
-else:
-    print("The cost of your trip is $10")
+# Distance = float(input("Enter the distance of your trip in miles: "))
+# if Distance <= 100:
+#     print("The cost of your trip is $5")
+# elif Distance <=500:
+#     print("The cost of your trip is $8")
+# else:
+#     print("The cost of your trip is $10")
 
 
 # Question 2
@@ -50,20 +50,260 @@ Write a program to calculte and display the ticket price.
 '''
 
 # Solution 2
-Age = int(input("Enter your age: "))
-Movie_Time = input("Enter the move time: (morning, afternoon or evening): ").lower()
-if Movie_Time == "morning":
-    print("The cost of your movie cost is #100")
+# Age = int(input("Enter your age: "))
+# Movie_Time = input("Enter the move time: (morning, afternoon or evening): ").lower()
+# if Movie_Time == "morning":
+#     print("The cost of your movie cost is #100")
 
-elif Movie_Time == "afternoon" and Age >= 18:
-    print("You're an adult, your movie cost is #150")
-elif Movie_Time == "evening" and Age >= 18:
-    print("You're an adult, your movie cost is #200")
-else:
-    print("You're a children, your movie cost is #100 ")
-
-
+# elif Movie_Time == "afternoon" and Age >= 18:
+#     print("You're an adult, your movie cost is #150")
+# elif Movie_Time == "evening" and Age >= 18:
+#     print("You're an adult, your movie cost is #200")
+# else:
+#     print("You're a children, your movie cost is #100 ")
 
 
 
 
+
+
+# Assignment 3
+
+# Union
+'''
+union of two sets creates a new set that contains all the distinct elements from both sets,
+without any duplicates.
+''' 
+# Example 1
+# set1 = {1, 2, 3, 4}
+# set2 = {3, 4, 5, 6}
+
+# union_set = set1.union(set2)
+
+# print(union_set)
+
+
+# Example 2
+
+# setA = {1, 2, 3}
+# setB = {3, 4, 5}
+
+# union_set = setA | setB
+
+# print(union_set)
+
+
+# Intersection
+'''
+It returns a new set containing all the elements that are present in all of the input sets.
+'''
+
+# Example 1
+
+# set1 = {1, 2, 3, 4, 5}
+# set2 = {3, 4, 5, 6}
+
+# intersection_set = set1.intersection(set2)
+
+# print(intersection_set)
+
+# Example 2
+
+# setA = {1, 2, 3, 4, 5}
+# setB = {3, 4, 5, 6}
+
+# intersection_set = setA & setB
+
+# print(intersection_set)
+
+
+# intersection update
+'''
+intersection update is used to update a set by keeping only the elements 
+that are common to the set and one or more other sets.
+'''
+
+# Example 1
+# set1 = {1, 2, "book", 4, 5}
+# set2 = {"book", 4, 5, 6}
+
+# set1.intersection_update(set2)
+
+# print(set1)
+
+# Example 2 
+# setA = {"people","food", "education", 9}
+# setB = {"girl", "boy", "food", 5, 9, "people"}
+
+# setB.intersection_update(setA)
+# print(setB)
+
+
+# symmetric difference
+'''
+symmetric difference is used to find the elements that are unique to each of the input sets 
+while excluding elements that are common to both sets.
+'''
+# Example 1
+# set1 = {1, 2, 3, 4, 5}
+# set2 = {3, 4, 5, 6}
+
+# sym_diff_set = set1.symmetric_difference(set2)
+
+# print(sym_diff_set)
+
+# Example 2
+# setA = {1, 2, 3, 4, 5}
+# setB = {3, 4, 5, 6}
+
+# sym_diff_set = setA ^ setB
+
+# print(sym_diff_set)
+
+
+# symmetric difference update
+'''
+symmetric difference update is used to update a set by keeping only the elements that are unique 
+to the set and one or more other sets, while excluding elements that are common to both sets. 
+'''
+# Example 1
+# set1 = {1, 2, 3, 4, 5}
+# set2 = {3, 4, 5, 6}
+
+# set1.symmetric_difference_update(set2)
+
+# print(set1)
+
+# Example 2
+# setA = {1, 2, "phone", 4, 5, "house"}
+# setB = {3, 4, 5, "house"}
+
+# setB.symmetric_difference_update(setA)
+
+# print(setB)
+
+
+# Difference
+'''
+difference is used to find the elements that are unique to one set, 
+excluding elements that are present in another set.
+'''
+# Example 1
+# set1 ={1, "good", 3, 9, 4, 5}
+# set2 = {3, 4, 5, 6, "come"}
+
+# difference_set = set1.difference(set2)
+
+# print(difference_set)
+
+
+# Example 2
+# setA = {1, "good", 3, 9, 4, 5}
+# setB = {3, 4, 5, 6, "come"}
+
+# difference_set = setA - setB
+
+# print(difference_set)
+
+
+# Difference Update
+'''
+difference update is used to update a set by removing elements that are also present in another set. 
+'''
+# Example 1
+# set1 = {1, 2, 3, 4, 5}
+# set2 = {3, 4, 5, 6}
+
+# set1.difference_update(set2)
+
+# print(set1)
+
+# # Example 2 
+# set1 = {"apple", 2, 3, 4, "banana"}
+# set2 = {3, 4, 5, "orange"}
+
+# set1.difference_update(set2)
+
+# print(set1)
+
+
+# Issubset
+'''
+issubset is used to check if one set is a subset of another set. 
+A subset is a set that contains only elements that are also present in the other set.
+'''
+# Example 1
+# set1 = {1, 2, 3}
+# set2 = {1, 2, 3, 4, 5}
+
+# # Check if set1 is a subset of set2
+# is_subset = set1.issubset(set2)
+
+# print(is_subset)
+
+# # Example 2
+# setA = {"power", 2, 3}
+# setB = {1, 2, 3, 4, 5}
+
+# # Check if set1 is a subset of set2
+# is_subset = setA.issubset(setB)
+
+# print(is_subset)
+
+
+# set1 = {"power", 2, 3}
+# set2 = {1, 2, 3, 4, 5}
+
+# # Check if set1 is a subset of set2
+# is_subset = set1 <= set2
+
+# print(is_subset)
+
+
+# Issuperset
+'''
+issuperset is used to check if one set is a superset of another set. 
+A superset is a set that contains all the elements of another set.
+'''
+
+# Example 1
+# set1 = {1, 2, 3, 4, 5}
+# set2 = {3, 4}
+
+# # Check if set1 is a superset of set2
+# is_superset = set1.issuperset(set2)
+
+# print(is_superset)
+
+# Example 2
+# set1 = {1, 2, 3, 4, 5}
+# set2 = {3, 4}
+
+# # Check if set1 is a superset of set2
+# is_superset = set1 <= set2
+
+# print(is_superset)
+
+
+# Isdisjoint
+'''
+isdisjoint is used to determine whether two sets are disjoint, 
+which means they have no elements in common.
+'''
+# Example 1
+set1 = {1, 2, 3}
+set2 = {4, 5, 6}
+
+# Check if set1 and set2 are disjoint
+is_disjoint = set1.isdisjoint(set2)
+
+print(is_disjoint)
+
+# Example 2
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+
+# Check if set1 and set2 are disjoint
+is_disjoint = set1.isdisjoint(set2)
+
+print(is_disjoint)
--- prac.py
+++ prac.py
@@ -1,55 +1,79 @@
-a = 3
-print(a)
+# a = 3
+# print(a)
 
-name = "Wakil"
-txt = "My name is {}"
-print(txt.format(name))
+# name = "Wakil"
+# txt = "My name is {}"
+# print(txt.format(name))
 
-price = 45
-txt = "The price is {} dollals"
-print(txt.format(price))
+# price = 45
+# txt = "The price is {} dollals"
+# print(txt.format(price))
 
-price = 45
-txt = "The price is {:.2f} dollars"
-print(txt.format(price))
+# price = 45
+# txt = "The price is {:.2f} dollars"
+# print(txt.format(price))
 
-quatity = 456
-product = "clothes"
-price = 50
-my_order = "I need {} pieces of {} for {:.2f} dollars"
-print(my_order.format(quatity, product, price))
-
-
-quatity = 456
-product = "clothes"
-price = 50
-my_order = "I need {0} pieces of {1} for {2} dollars"
-print(my_order.format(quatity, product, price))
-
-quatity = 456
-product = "clothes"
-price = 50
-my_order = "I need {0} pieces of {1} for {2:.2f} dollars"
-print(my_order.format(quatity, product, price))
-
-#string Interpolation
-name = "Alice"
-age = 30 
-message = f"My name is {name} and I am {age} years old."
-print(message)
-
-#str.format() method
-name = "Alice"
-age = 30
-print("My name is {} and I am {} years old.".format(name, age))
-
-#Old-Style String Formatting ( % Operator)
-name = "Alice"
-age = 30
-message = "My name is %s and I am %d years old." % (name, age)
+# quatity = 456
+# product = "clothes"
+# price = 50
+# my_order = "I need {} pieces of {} for {:.2f} dollars"
+# print(my_order.format(quatity, product, price))
 
 
-name = input("Enter your name: ")
-age = input("Enter your age: ")
-email_msg = f"Hello {name}, you are {age} years old."
-print(email_msg)
+# quatity = 456
+# product = "clothes"
+# price = 50
+# my_order = "I need {0} pieces of {1} for {2} dollars"
+# print(my_order.format(quatity, product, price))
+
+# quatity = 456
+# product = "clothes"
+# price = 50
+# my_order = "I need {0} pieces of {1} for {2:.2f} dollars"
+# print(my_order.format(quatity, product, price))
+
+# #string Interpolation
+# name = "Alice"
+# age = 30 
+# message = f"My name is {name} and I am {age} years old."
+# print(message)
+
+# #str.format() method
+# name = "Alice"
+# age = 30
+# print("My name is {} and I am {} years old.".format(name, age))
+
+# #Old-Style String Formatting ( % Operator)
+# name = "Alice"
+# age = 30
+# message = "My name is %s and I am %d years old." % (name, age)
+
+
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# email_msg = f"Hello {name}, you are {age} years old."
+# print(email_msg)
+
+
+# # A simple todo -list app
+
+# # create an empty todo list
+
+
+# a = [2, 5, 3, 4]
+# a[2:2]=[2]
+# print(a)
+
+# lis = [[8,7], [6,5]]
+# for p, q in lis:
+#     print(p + q, end="&")
+
+# s = "clcoding"
+# x = slice(1, 4)
+# print(s[x])
+
+nums = [42, 8, 3, 17, 8]
+nums.remove(8)
+nums.sort()
+print(nums)
+print(''.join(str(nums)[1:-1]))
